ea3b2e7900e8e1cda91ed0ca7eb75538d28a45fd
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-06 13:04:17 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -120,7 +120,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_id
-  iam_instance_profile = aws_iam_instance_profile.mysql.name
+ 
   
   tags = merge(
     {
