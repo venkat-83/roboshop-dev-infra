f21a614c9d3054b0ce49e98f7971169b8201358a
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-06 12:00:56 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,5 +1,6 @@
 locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     common_name = "${var.project}-${var.environment}"
     
     ami_id = data.aws_ami.joindevops.id
