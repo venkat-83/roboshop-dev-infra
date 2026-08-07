e117a1725f5e6c4e01fbb264de0b07153a338557
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-07 15:38:25 +0530

    terraform

--- 40-databases/provider.tf
+++ 40-databases/provider.tf
@@ -9,7 +9,7 @@ terraform {
   # remote state storage location
   backend "s3" {
     bucket         = "remote-state-33"
-    key            = "roboshop-data1.tfstate"
+    key            = "roboshop-data2.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true # Enables native S3 state locking (Terraform 1.10+)
