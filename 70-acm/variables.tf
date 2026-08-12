308f72923a0cb49eb12fec2c26783e08e78c6063
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-12 16:43:44 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "devops90.online"
+}
+
+variable "zone_id" {
+    default = "Z09020561TNJYXT41O3YZ"
+}  
