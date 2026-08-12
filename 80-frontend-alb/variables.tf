50395ca7f96a9b878811335a19f1886c6611004f
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-12 17:29:02 +0530

    terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09020561TNJYXT41O3YZ"
+}  
+
+variable "domain_name" {
+    default = "devops90.online"
+}
