ecb5524f737f30a76fd29cffccd72cf44fccadc1
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-12 15:22:23 +0530

    terraform

---
+++ 60-catalogue/variable.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+variable "zone_id" {
+    default = "Z09020561TNJYXT41O3YZ"
+}   
+
+variable "domain_name" {
+    default = "devops90.online"
+}
