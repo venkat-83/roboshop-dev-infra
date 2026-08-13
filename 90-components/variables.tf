eb03b90c9e89ddb784b0b1b649e3a477ded46849
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-13 16:03:09 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -14,8 +14,24 @@ variable "domain_name" {
 variable "components" {
     default = {
         catalogue = {
+            rule_priority = 10
+            app_version = "v3"
+        }
+        user = {
+            rule_priority = 20
+            app_version = "v3"
+        }
+        cart = {
+            rule_priority = 30
+            app_version = "v3"
+        }
+         shipping = {
+            rule_priority = 40
+            app_version = "v3"
+        }
+         payment = {
+            rule_priority = 50
             app_version = "v3"
         }
     }
-
 }
