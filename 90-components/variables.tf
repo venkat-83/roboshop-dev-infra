468b66373c363dc1c4b8a8a8e52b77c4fbc6e34f
venkat-83 <venkatasaigurla83@gmail.com>
2026-08-13 17:17:31 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -25,12 +25,12 @@ variable "components" {
             rule_priority = 30
             app_version = "v3"
         }
-         shipping = {
-            rule_priority = 40
+        payment = {
+            rule_priority = 50
             app_version = "v3"
         }
-         payment = {
-            rule_priority = 50
+        frontend = {
+            rule_priority = 10
             app_version = "v3"
         }
     }
